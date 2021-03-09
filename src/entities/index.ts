82f097fb28cc76042003d2c708bc9434e88d4484
tsugehara <tsugehara@gmail.com>
2021-03-09 12:51:38 +0900

    細かい更新 (#2)
    * 小さな更新
    
    * 0.1.1
    
    * add basic button

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,2 @@
 export { default as RedRect } from "./RedRect";
+export { default as FrameButton, FrameButtonParameters } from "./FrameButton";
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,22 +1,52 @@
 import * as types from "../types";
-import BaseScene from "./BaseScene";
 import * as entities from "../entities";
 import * as handlers from "../handlers";
 import * as behaviors from "../behaviors";
+import * as components from "../components";
+import * as utils from "../utils";
+import BaseScene from "./BaseScene";
 
 export default class extends BaseScene {
+	button?: components.BasicButton;
+
+	rect?: entities.RedRect;
+
 	constructor(param: g.SceneParameterObject) {
-		super(param);
+		super(utils.mergeAssetPaths(param, ["/assets/parts/buttons.png"]));
 
 		this.onLoad.addOnce(this.onLoadHandler, this);
 		this.onMessage.add(handlers.LogginHandler);
 		this.onMessage.add(this.onMessageHandler, this);
+		this.button = undefined;
+		this.rect = undefined;
 	}
 
 	onLoadHandler() {
-		const rect = new entities.RedRect(this);
-		new behaviors.LoopMoveToRight(rect).activate();
-		this.append(rect);
+		this.rect = new entities.RedRect(this);
+		new behaviors.LoopMoveToRight(this.rect).activate();
+
+		this.button = new components.BasicButton({
+			scene: this,
+			src: this.asset.getImage("/assets/parts/buttons.png"),
+			width: 34,
+			height: 34,
+			x: this.game.width / 2 - 34 / 2,
+			y: this.game.height - 34 - 17,
+			parent: this,
+		});
+		this.button.clicked.add(this.onButtonClick, this);
+
+		this.append(this.rect);
+	}
+
+	onButtonClick() {
+		if (this.rect.visible()) {
+			this.rect.hide();
+		} else {
+			this.rect.show();
+		}
+		this.rect.modified();
+		console.log("clicked");
 	}
 
 	onMessageHandler(arg: types.MessageEvent) {
